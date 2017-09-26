68738dd6e383ee1177635bc4b93b2ed4b25f0b83
aziz lahkimi <lahkimi.maziz@gmail.com>
2017-09-26 14:56:32 +0100

    upgrade laravel and voyager

--- app/User.php
+++ app/User.php
@@ -4,8 +4,9 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use TCG\Voyager\Models\User as VoyagerUser;
 
-class User extends Authenticatable
+class User extends VoyagerUser
 {
     use Notifiable;
 
